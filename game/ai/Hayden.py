821180bbce5ff7d5ee67b1b6c5b760daf209c008
Hayden Henrie <hadesroc@gmail.com>
2015-11-12 16:54:33 -0700

    Stubbed out a HaydenBot

--- game/ai/Hayden.py
+++ game/ai/Hayden.py
@@ -1,17 +1,10 @@
 import random
-from game.bot import Bot, Actions
-__author__ = 'Davin'
+from game.bot import Bot,  Actions
 
-# class BoringBot(Bot):
-#     """Walk in a square and do nothing."""
-#     def update(self, tick_number, visible_objects):
-#         print(visible_objects)
-#         if tick_number % 3:
-#             return Actions.MoveForward
-#         return Actions.TurnLeft
-#
-#
-# class LazyBot(Bot):
-#     """Walk in a square and do nothing."""
-#     def update(self, tick_number, visible_objects):
-#         return Actions.DoNothing
+
+class Haydenbot (Bot):
+	def update(self, tick_number, visible_objects):
+		if tick_number % 3:
+			return Actions.MoveForward
+		return Actions.TurnLeft
+		
