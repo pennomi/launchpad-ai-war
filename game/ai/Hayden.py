84f91b07828ea128c725af47871a934516142faf
BaracudaMonkey <warnick.ammon@gmail.com>
2015-11-12 17:05:15 -0700

    Fixed merge conflict

--- game/ai/Hayden.py
+++ game/ai/Hayden.py
@@ -1,21 +1,6 @@
 import random
 from game.bot import Bot,  Actions
 
-<<<<<<< HEAD
-class BoringBot(Bot):
-    """Walk in a square and do nothing."""
-    def update(self, tick_number, visible_objects):
-        print(visible_objects)
-        if tick_number % 3:
-            return Actions.MoveForward
-        return Actions.TurnLeft
-
-
-class LazyBot(Bot):
-    """Walk in a square and do nothing."""
-    def update(self, tick_number, visible_objects):
-        return Actions.DoNothing
-
 
 class Haydenbot (Bot):
 	def update(self, tick_number, visible_objects):
