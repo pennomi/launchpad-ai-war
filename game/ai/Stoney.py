d1923dfa6b137c376eca99661062e2489f5d3939
stoneycarson <area51guy@live.com>
2015-11-16 16:30:32 -0800

    Updated CheeseBot
    some AI stuff

--- game/ai/Stoney.py
+++ game/ai/Stoney.py
@@ -9,20 +9,31 @@ class CheeseBot(Bot):
     closest = None
 
     def update(self, tick_number, visible_objects):
+        if self.closest == None:
+            self.getClosest(visible_objects)
+        else:
+            return self.attackClosest()
+        return Actions.DoNothing
 
-        if visible_objects:
-            return Actions.MoveForward
-
-        return Actions.TurnLeft
-
-    def scan(self, visible_objects):
+    def getClosest(self, visible_objects):
         if visible_objects:
             for v in visible_objects:
-                if v.getPosition() - self.get_direction() < self.closest:
-                    self.closest = v;
+                if v.get_position() - self.get_direction() < self.closest:
+                    self.closest = v
         else:
             return Actions.TurnLeft
-        
+
+    def attackClosest(self):
+        p = self.closest.get_position()
+        d = self.closest.get_direction()
+        mep = self.get_position()
+        med = self.get_direction()
+        if mep - p < 2 and d == p - med:
+            return Actions.DoNothing
+
+        return Actions.Punch
+
+
 
 
 
